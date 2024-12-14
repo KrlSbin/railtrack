b37c773bd0e485e97553bcc56280c1020c27d141
Kyrylo Skriabin <ckp9l6ih@gmail.com>
2024-12-15 00:38:19 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,4 +10,5 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
+cp config/database.yml.distr config/database.yml
 bundle exec rails db:migrate
